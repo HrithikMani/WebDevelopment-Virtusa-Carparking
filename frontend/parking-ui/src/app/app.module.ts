7e26a7b7c74e68526105cd304434223b1cbb0e60
Hrithik Mani <hrihtik.mani2000@gmail.com>
2021-03-26 16:23:39 +0530

    Feat:Adding LoginPage (Login form component, home nav bar)

--- frontend/parking-ui/src/app/app.module.ts
+++ frontend/parking-ui/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { HomeHeaderComponent } from './home-header/home-header.component';
+import { HomeFooterComponent } from './home-footer/home-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LoginFormComponent,
+    HomeHeaderComponent,
+    HomeFooterComponent
   ],
   imports: [
     BrowserModule,
