dfab4b7ee7ccde31d593ae063213b2a981e0e472
Hrithik Mani <hrihtik.mani2000@gmail.com>
2021-03-26 14:47:19 +0530

    Project Basic Structure setup

---
+++ backend/carpoolapi/carpoolapi/src/main/java/com/example/carpoolapi/CarpoolapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.carpoolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpoolapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpoolapiApplication.class, args);
+	}
+
+}
